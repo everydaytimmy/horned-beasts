0bea043a34ab3558903883d3e6de8e50a5aef156
Timothy Viccari <tjvicc84@gmail.com>
2021-04-13 17:12:24 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, CardColumns, Card, Navbar } from 'react-bootstrap'
 import Main from './components/main.js';
 import Footer from './components/footer.js';
 import Header from './components/header.js';
+import item from './data.json';
 
 function App() {
   return (
--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,17 @@
+import { NavItem } from "react-bootstrap";
 
 function Main() {
 
-  return <HornedBeasts title={'Rhino'} description="Thicc Unicorn" />
+  return <HornedBeasts title={'Rhino'} description="Thicc Unicorn"/>
 
 }
 
 function HornedBeasts(props) {
   return (
     <div>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+
       <h1>{props.title}</h1>
       <p>{props.description}</p>
     </div>
