226f7e7add36afd2848e93a88dd06b6746e0a24f
Timothy Viccari <tjvicc84@gmail.com>
2021-04-14 17:43:02 -0400

    changes to  class components

--- src/components/horned.js
+++ src/components/horned.js
@@ -6,7 +6,7 @@ class HornedBeasts extends React.Component {
 
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '400px' }}>
         <Card.Img variant="top" src={this.props.img} />
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
