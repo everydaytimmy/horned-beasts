807bd7abef4651bcf74aedf76ba0f76939d9edbd
‘Timothy <Timothy.viccari@gmail.com>
2021-04-17 19:31:47 -0400

    added form

--- src/components/beastform.js
+++ src/components/beastform.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 
+class BeastForm extends React.Component {
 
-class BeastForm extends React.Component{
 
+  render() {
+    return (
+      <Form>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Number of Horns</Form.Label>
+          <Form.Control as="select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>WOW</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+    );
+  }
 
-    render() {
-      return(
-        <form>
-          <input placeholder="number of horns" type="text" />
-        </form>
-      );
-    }
+}
 
-} 
+export default BeastForm;
--- src/components/main.js
+++ src/components/main.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import HornedBeasts from "./horned.js"
+import BeastForm from "./beastform.js";
 import CardColumns from 'react-bootstrap/CardColumns';
 import item from '../data.json';
+
 
 class Main extends React.Component {
 
   render() {
     return (
       <>
+        <BeastForm />
         <CardColumns>
           {
-            item.map((element, index) => 
-            <HornedBeasts 
-            title={element.title} 
-            description={element.description} 
-            img={element.image_url} 
-            />)
+            item.map((element, index) =>
+              <HornedBeasts
+                title={element.title}
+                description={element.description}
+                img={element.image_url}
+              />)
           }
         </CardColumns>
       </>
